b1c3bad7c6256bcf44429e3aebe9125c07cbd4cc
Jiali Huang <57115077+kyliehuangjh@users.noreply.github.com>
2019-10-28 19:39:25 -0700

    Update show2.js

--- SampleFiles/scripts/show2.js
+++ SampleFiles/scripts/show2.js
@@ -1,2 +1,17 @@
 console.log(tweets)
+const tweet2 function() {
+    //TODO: output the contents of the second tweet below the first one:
+
+    document.querySelector('#tweet2 .message').innerHTML = tweets.statuses[1].text
+    document.querySelector('#tweet2 .username').innerHTML = tweets.statuses[1].user.screen_name
+    document.querySelector('#tweet2 .date').innerHTML = tweets.statuses[1].created_at
+
+}
+
+const tweet3 = () => {
+    //TODO: output the contents of the third tweet below the second one:
+    document.querySelector('#tweet1 .message').innerHTML = tweets.statuses[2].text
+    document.querySelector('#tweet1 .username').innerHTML = tweets.statuses[2].user.screen_name
+    document.querySelector('#tweet1 .date').innerHTML = tweets.statuses[2].created_at
+}
 
